8a30fa4f9f5eb8527d0bf6a1a169a481f04b8a6e
naseer <naseercs91@gmail.com>
2014-04-09 15:51:34 +0530

    working setting time and waking alarm clock

--- src/com/toprecur/myalarm/MainActivity.java
+++ src/com/toprecur/myalarm/MainActivity.java
@@ -1,15 +1,13 @@
 package com.toprecur.myalarm;
 
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemClock;
+import android.provider.AlarmClock;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -19,74 +17,96 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-
-	protected static final TimePicker timePicker1 = null;
-	protected static final Calendar calendar = null;
-	protected static final String ELAPSED_TIME = null;
-	protected static final String INTERVAL_DAY = null;
-	protected static final PendingIntent operation = null;
-	protected String FLAG_ACTIVITY_NEW_TASK;
-	protected Context context;
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		 
-	
-	            	final AlarmManager alarmManager = (AlarmManager) getBaseContext().getSystemService(ALARM_SERVICE);
-	            	 OnClickListener setClickListener = new OnClickListener() {
-	              public void onClick(View v) {
-	                TimePicker tpTime = (TimePicker) findViewById(R.id.tp_time);
-	 
-	                int hour = tpTime.getCurrentHour();
-	                int minute = tpTime.getCurrentMinute();
-              
-	                GregorianCalendar calendar = new GregorianCalendar( hour, minute, 0);
-	 	             
-	            	
-	                long alarm_time = calendar.getTimeInMillis();
-	                
-	                Intent intent = new Intent(MainActivity.this,SoundActivity.class);
-	                
-	                PendingIntent operation = PendingIntent.getActivity(getBaseContext(), 0, intent, Intent.FLAG_ACTIVITY_NEW_TASK);
-	                	               
-	                Intent intent1 = new Intent(MainActivity.this,RepeatActivity.class);
-	                
-	                alarmManager.set(AlarmManager.RTC_WAKEUP  , alarm_time , operation);
-	                
-	                Toast.makeText(getBaseContext(), "Alarm is set successfully",Toast.LENGTH_SHORT).show();
-	             
-	            }
-	        };
-	         OnClickListener repeatClickListener = new OnClickListener (){
-	        	 public void onClick (View v){
-	        		 alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime(),
-	                         2*60*60, operation);
-	        	 }
-	         };
-	        	
-	        		
-	        OnClickListener quitClickListener = new OnClickListener() {
-	            @Override
-	            public void onClick(View v) {
-	            	
-	                finish();
-	            }
-	        };
-	 
-	        Button btnSetAlarm = ( Button ) findViewById(R.id.btn_set_alarm);
-	        btnSetAlarm.setOnClickListener(setClickListener);
-	 
-	        Button btnQuitAlarm = ( Button ) findViewById(R.id.btn_quit_alarm);
-	        btnQuitAlarm.setOnClickListener(quitClickListener);
-	        
-	        Button btnRepeatAlarm = (Button) findViewById(R.id.btn_set_repeat);
-	        btnRepeatAlarm.setOnClickListener (setClickListener);
-	       
-	        }
 
-	
+		
+		OnClickListener setClickListener = new OnClickListener() {
+			
+
+			public void onClick(View v) {
+
+				TimePicker tpTime = (TimePicker) findViewById(R.id.tp_time);
+		
+//				 GregorianCalendar calendar = new GregorianCalendar(hour,
+//						 minute, 0);
+				AlarmManager alarmManager = (AlarmManager) getBaseContext()
+						.getSystemService(ALARM_SERVICE);
+				
+				int hour = tpTime.getCurrentHour();
+				int minute = tpTime.getCurrentMinute();
+				
+				Calendar calendar = Calendar.getInstance();
+				calendar.setTimeInMillis(System.currentTimeMillis());
+				long alarm_time = calendar.getTimeInMillis();
+			
+			/*	StringBuilder sb = new StringBuilder();
+				sb.append(hour);
+				sb.append(minute);
+				
+				String str = Integer.toString(hour);
+				String str1 = Integer.toString(minute);
+						
+				intent.putExtra(str, hour);
+				intent.putExtra(str1, minute);*/
+
+			//	Intent intent = new Intent(MainActivity.this,SoundActivity.class);
+				 Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+		            //.putExtra(AlarmClock.EXTRA_MESSAGE, message)
+		            .putExtra(AlarmClock.EXTRA_HOUR, hour)
+		            .putExtra(AlarmClock.EXTRA_MINUTES, minute);
+		   
+		        startActivity(intent);
+				
+				PendingIntent operation = PendingIntent.getActivity(
+						getBaseContext(), 0, intent,Intent.FLAG_ACTIVITY_NEW_TASK);
+			  //intent.putExtra ("hour",hour);
+			  //intent.putExtra ("minute",minute);
+			  //tartActivity(intent);
+				alarmManager.set(AlarmManager.RTC_WAKEUP,
+						calendar.getTimeInMillis(), operation);
+
+				Toast.makeText(MainActivity.this, "Alarm is set successfully",
+						Toast.LENGTH_SHORT).show();
+
+			}
+
+		};
+
+		 OnClickListener repeatClickListener = new OnClickListener() {
+		 public void onClick(View v) {
+		 Intent intent = new Intent(MainActivity.this,RepeatActivity.class);
+		
+		  intent = putExtra(this, RepeatActivity.class);
+		  startActivity(intent);
+		 }
+		
+		 private Intent putExtra(OnClickListener onClickListener,
+		 Class<RepeatActivity> class1) {
+		
+		 return null;
+		 }
+		 };
+
+		OnClickListener quitClickListener = new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				finish();
+			}
+		};
+		Button btnSetAlarm = (Button) findViewById(R.id.btn_set_alarm);
+		btnSetAlarm.setOnClickListener(setClickListener);
+		
+		Button btnQuitAlarm = (Button) findViewById(R.id.btn_quit_alarm);
+		btnQuitAlarm.setOnClickListener(quitClickListener);
+
+//		Button btnRepeatAlarm = (Button) findViewById(R.id.btn_set_repeat);
+//		btnRepeatAlarm.setOnClickListener(setClickListener);
+
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- src/com/toprecur/myalarm/SoundActivity.java
+++ src/com/toprecur/myalarm/SoundActivity.java
@@ -6,25 +6,36 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.AlarmClock;
 import android.view.Menu;
 
 public class SoundActivity extends Activity {
-	
+
 	private MediaPlayer mPlay;
 	public Intent intent;
+
+//	String str;
+//	String str1;
+	int hour;
+	int minute;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sound);
-		
-		MediaPlayer mPlay = MediaPlayer.create(this,R.raw.alarm_rooster);
+		intent.getIntExtra("AlarmClock.EXTRA_HOUR",hour);
+		intent.getIntExtra("AlarmClock.EXTRA_MINUTES", minute);
+//		intent.getIntExtra("hour", 0);
+//		intent.getIntExtra("minute",0);
+//		int hour = Integer.parseInt(str);
+//		int minute = Integer.parseInt(str1);
+	
+		MediaPlayer mPlay = MediaPlayer.create(this, R.raw.alarm_rooster);
 		mPlay.start();
-		Uri soundURI = Uri
-				.parse("android.resource://course.examples.Notification.StatusBarWithCustomView/"
-						+ R.raw.alarm_rooster);
+		Uri soundURI = Uri.parse("android.resource://com.toprecur.myalarm/"
+				+ R.raw.alarm_rooster);
 		Notification.Builder notificationBuilder = new Notification.Builder(
 				getApplicationContext()).setSound(soundURI);
-		
+
 	}
 
 	@Override
